34e9a8e8bce56f00d6f2ce3952fa555967b8b268
Md Ariful Hossain <ariful15-12473@diu.edu.bd>
2022-06-05 00:08:10 +0600

    Run this file to start the game

---
+++ Main_Class.java
@@ -0,0 +1,10 @@
+
+package mission.of.aeroplane;
+
+public class Main_Class {
+    
+    public static void main(String[] args) {
+        GameSetUp Game=new GameSetUp("AirPlane Fighter",650,750);
+        Game.start();
+    }
+}
